288d9455b74eb1c44a21d8df15102bbd91ba84b4
Camille <camille.guardiana123@gmail.com>
2026-03-23 18:42:58 +0800

    Implement TaskController with CRUD REST endpoints

--- src/main/java/com/camille/taskmanager_api/TaskController.java
+++ src/main/java/com/camille/taskmanager_api/TaskController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
     private final TaskManager manager;
 
