69eed8e896a6891fc8a38f02bd14cd746ea740ba
Camille <camille.guardiana123@gmail.com>
2026-03-24 13:08:54 +0800

    Refactor: Move business logic to TaskService and clean TaskController

--- src/main/java/com/camille/taskmanager_api/TaskController.java
+++ src/main/java/com/camille/taskmanager_api/TaskController.java
@@ -9,52 +9,43 @@ import java.util.List;
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
-    private final TaskManager manager;
+    private final TaskService service;
 
-    public TaskController() {
-        this.manager = new TaskManager();
-        manager.loadTasks("tasks.txt");
+    public TaskController(TaskService service) {
+        this.service = service;
     }
 
     // GET /tasks → list all tasks
     @GetMapping
-    public List<Task> getTasks() {
-        return manager.getTasks();
+    public ResponseEntity<List<Task>> getTasks() {
+        List<Task> tasks = service.getTasks();
+        return ResponseEntity.ok(tasks);
     }
 
     // POST /tasks → add a new task
     @PostMapping
-    public Task addTask(@RequestBody Task task) {
-        // Reuse TaskManager Logic
-        Task newTask = manager.addTask(task.getName(), task.getDescription());
-        manager.saveTasks("tasks.txt");
-        return newTask;
+    public ResponseEntity<Task> addTask(@RequestBody Task task) {
+        Task newTask = service.addTask(task.getName(), task.getDescription());
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTask);
     }
 
     // PUT /tasks/{id} → update a task
     @PutMapping("/{id}")
     public ResponseEntity <Task> updateTask(@PathVariable int id, @RequestBody Task task) {
-        boolean updated = manager.editTask(id, task.getName(), task.getDescription());
-        if (updated){
-            manager.saveTasks("tasks.txt");
-            Task updatedTask =  manager.getTasks().stream()
-                    .filter(t -> t.getId() == id)
-                    .findFirst()
-                    .orElse(null);
-            return ResponseEntity.ok(updatedTask);
+        Task updateTask = service.updateTask(id, task.getName(), task.getDescription());
+        if(updateTask != null) {
+            return ResponseEntity.ok(updateTask);
         }else {
             return ResponseEntity.notFound().build();
         }
-
     }
 
     // DELETE /tasks/ {id} -> remove task
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteTask(@PathVariable int id) {
-        boolean removed = manager.deleteTask(id);
+        boolean removed = service.deleteTask(id);
 
         if (removed){
-            manager.saveTasks("tasks.txt");
             return ResponseEntity.ok("Task removed successfully");
         }else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task not found");
