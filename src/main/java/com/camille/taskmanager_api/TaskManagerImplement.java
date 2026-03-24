513002ad2f8dd591b344942ea704ba1d917391f5
Camille <camille.guardiana123@gmail.com>
2026-03-24 23:27:33 +0800

    Refactor: Introduce TaskManager interface with multiple implementations (TaskManagerImpl, InMemoryTaskManager)

--- src/main/java/com/camille/taskmanager_api/TaskManagerImplement.java
+++ src/main/java/com/camille/taskmanager_api/TaskManagerImplement.java
@@ -1,50 +1,154 @@
 package com.camille.taskmanager_api;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
+
+@Component
+@Primary
 public class TaskManagerImplement implements TaskManager {
-    @Override
+    ArrayList<Task> tasks;
+    private int nextId = 1;
+
+    // Constructor
+    public TaskManagerImplement() {
+        tasks = new ArrayList<>();
+    }
+
     public ArrayList<Task> getTasks() {
-        return null;
+        return tasks;
     }
 
-    @Override
+    // Add a new task or update
     public Task addTask(String name, String description) {
-        return null;
+        Task task = new Task(nextId, name, description);
+        if (name == null || name.isEmpty()) {
+            return null;
+        }
+
+        if (description.length() > 100) {
+            description = description.substring(0, 100);
+        }
+
+        tasks.add(task);
+        System.out.println("Task added: " + task);
+        nextId++;
+
+        return task;
     }
 
-    @Override
     public boolean markTaskCompleted(int taskId) {
+        for (Task task : tasks) {
+            if (task.getId() == taskId) {
+                task.markAsCompleted();
+                return true;
+            }
+        }
         return false;
     }
 
-    @Override
     public ArrayList<Task> getCompletedTasks() {
-        return null;
+        ArrayList<Task> completed = new ArrayList<>();
+
+        for (Task task : tasks) {
+            if (task.isCompleted()) {
+                completed.add(task);
+            }
+        }
+
+        return completed;
     }
 
-    @Override
     public ArrayList<Task> getPendingTasks() {
-        return null;
+        ArrayList<Task> pending = new ArrayList<>();
+        for (Task task : tasks) {
+            if (!task.isCompleted()) {
+                pending.add(task);
+            }
+        }
+
+        return pending;
     }
 
-    @Override
+
     public boolean deleteTask(int taskId) {
-        return false;
+        return tasks.removeIf(task -> task.getId() == taskId);
     }
 
-    @Override
     public boolean editTask(int taskId, String newName, String newDescription) {
+        for (Task task : tasks) {
+            if (task.getId() == taskId) {
+                //Update name of the task only if the new value is provided
+                if (newName != null && !newName.trim().isEmpty()) {
+                    task.setName(newName);
+                }
+
+                if (newDescription != null && !newDescription.trim().isEmpty()) {
+                    task.setDescription(newDescription);
+                }
+
+                return true;
+            }
+        }
         return false;
     }
 
-    @Override
     public void loadTasks(String filename) {
+        try (Scanner scanner = new Scanner(new File(filename))) {
+            tasks.clear();
+            int maxId = 0;
 
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+
+                String[] parts = line.split("\\|");
+
+                if (parts.length == 4) {
+                    int id = Integer.parseInt(parts[0]);
+                    String name = parts[1];
+                    String description = parts[2];
+                    boolean isCompleted = Boolean.parseBoolean(parts[3]);
+
+                    Task task = new Task(id, name, description);
+
+                    if (isCompleted) {
+                        task.markAsCompleted();
+                    }
+
+                    tasks.add(task);
+
+                    if (id > maxId) {
+                        maxId = id;
+                    }
+
+                }
+            }
+
+            nextId = maxId + 1;
+            System.out.println("Task loaded!");
+        } catch (Exception e) {
+            System.out.println("Error loading tasks!" + e.getMessage());
+        }
     }
 
-    @Override
     public void saveTasks(String filename) {
-
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filename))) {
+            for (Task task : tasks) {
+                writer.println(task.getId() + "|" +
+                        task.getName() + "|" +
+                        task.getDescription() + "|" +
+                        task.isCompleted());
+            }
+            System.out.println("Tasks saved to " + filename);
+        } catch (IOException e) {
+            System.out.println("Error saving tasks: " + e.getMessage());
+        }
     }
 }
