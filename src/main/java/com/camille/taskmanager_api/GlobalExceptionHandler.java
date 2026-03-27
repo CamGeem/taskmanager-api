d52a9ce533373a763f84264b4edf7ac13dc107d7
Camille <camille.guardiana123@gmail.com>
2026-03-27 18:17:41 +0800

    Updated GlobalExceptionHandler to handle 404 responses

--- src/main/java/com/camille/taskmanager_api/GlobalExceptionHandler.java
+++ src/main/java/com/camille/taskmanager_api/GlobalExceptionHandler.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<String> handleTaskNotFound(TaskNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleBadRequest (IllegalArgumentException e) {
+    public ResponseEntity<String> handleBadRequest (TaskNotFoundException e) {
         if (e.getMessage().contains("not found")) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
