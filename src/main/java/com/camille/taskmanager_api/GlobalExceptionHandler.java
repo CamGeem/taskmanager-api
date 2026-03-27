1191a3bc33ecf942c0ffa0ddf6cf905fe01b1ada
Camille <camille.guardiana123@gmail.com>
2026-03-27 10:30:56 +0800

    Rely on GlobalExceptionHandler for responses

--- src/main/java/com/camille/taskmanager_api/GlobalExceptionHandler.java
+++ src/main/java/com/camille/taskmanager_api/GlobalExceptionHandler.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleBadRequest (IllegalArgumentException e) {
+        if (e.getMessage().contains("not found")) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
